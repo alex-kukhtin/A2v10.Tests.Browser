6542535ec941f812f4135a94152102974b43b9e8
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-08-13 19:59:27 +0300

    v 7008

---
+++ A2v10.Tests.Browser/Xaml/Steps/Confirm.cs
@@ -0,0 +1,18 @@
+﻿// Copyright © 2019 Alex Kukhtin. All rights reserved.
+
+using System;
+using System.Threading;
+using System.Windows.Markup;
+
+namespace A2v10.Tests.Browser.Xaml
+{
+	[ContentProperty("Steps")]
+	public class Confirm : Dialog
+	{
+		public override void Run(IWebBrowser browser, IScope scope)
+		{
+			Thread.Sleep(100); // animation
+			base.Run(browser, scope);
+		}
+	}
+}
