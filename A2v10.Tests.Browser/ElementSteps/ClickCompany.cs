43e93ab1fd575e18a87fb6a81c4e26054436156b
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-12-02 14:37:54 +0200

    v 7022

--- A2v10.Tests.Browser/ElementSteps/ClickCompany.cs
+++ A2v10.Tests.Browser/ElementSteps/ClickCompany.cs
@@ -20,7 +20,7 @@ namespace A2v10.Tests.Browser.Xaml
 			String xPathItem = $".//a[contains(@class,'dropdown-item')]//span[contains(@class,'company-menu-name')][normalize-space()={Text.XPathText()}]/..";
 			var menuItem = comp.GetElementByXPath(xPathItem);
 
-			var icon = menuItem.GetElementByXPath("./i[contains(@class, 'ico')]");
+			var icon = menuItem.GetElementByXPath("./i[contains(@class, 'ico')]", checkVisibility:false);
 			var cls = icon.GetAttribute("class");
 			if (cls.Contains("ico-check"))
 			{
