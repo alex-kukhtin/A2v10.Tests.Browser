55cb7bbf3f08c499f2f564fbc49f87af082ac7eb
alex-kukhtin <alex.kukhtin@outlook.com>
2023-01-19 23:05:27 +0200

    v 7047

--- A2v10.Tests.Runner/Properties/AssemblyInfo.cs
+++ A2v10.Tests.Runner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oleksandr Kukhtin")]
 [assembly: AssemblyProduct("A2v10.Tests.Runner")]
-[assembly: AssemblyCopyright("Copyright © 2019-2022 Oleksandr Kukhtin. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2019-2023 Oleksandr Kukhtin. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.7046.0")]
-[assembly: AssemblyFileVersion("10.0.7046.0")]
+[assembly: AssemblyVersion("10.0.7047.0")]
+[assembly: AssemblyFileVersion("10.0.7047.0")]
